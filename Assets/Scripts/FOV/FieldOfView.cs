54b7ce64d7d76480c33b91fb652ece2e0ed92c56
HarshithAdepu <adepuharshith1608@gmail.com>
2021-12-06 15:17:00 +0530

    Implemented visibility cone except it's not working

---
+++ Assets/Scripts/FOV/FieldOfView.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius = 5;
+    public float viewAngle = 135;
+    Collider2D[] playerInRadius;
+    public LayerMask obstacleMask, playerMask;
+    public List<Transform> visiblePlayer = new List<Transform>();
+    private void FixedUpdate()
+    {
+        FindVisiblePlayer();
+    }
+
+    void FindVisiblePlayer()
+    {
+        playerInRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius);
+
+        visiblePlayer.Clear();
+
+        for(int i =0; i<playerInRadius.Length; i++)
+        {
+            Transform player = playerInRadius[i].transform;
+            Vector2 dirPlayer = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+            if(Vector2.Angle(dirPlayer, transform.right)<viewAngle/2)
+            {
+                float distancePlayer = Vector2.Distance(transform.position, player.position);
+
+                if(Physics2D.Raycast(transform.position, dirPlayer, distancePlayer, playerMask))
+                {
+                    visiblePlayer.Add(player);
+                }
+            }
+        }
+    }
+
+    public Vector2 DirFromAngle(float angleDeg, bool global)
+    {
+        if(!global)
+        {
+            angleDeg += transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Cos(angleDeg * Mathf.Deg2Rad), Mathf.Sin(angleDeg * Mathf.Deg2Rad));
+    }
+}
